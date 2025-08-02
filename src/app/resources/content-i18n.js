5dca5baf6b4108064ba56c3a50dcca872c3ad3b0
caldo <calderteoo@gmail.com>
2025-08-03 01:41:57 +0800

    fix: add missing name field to metadata

--- src/app/resources/content-i18n.js
+++ src/app/resources/content-i18n.js
@@ -2,15 +2,15 @@ import { InlineCode } from "@/once-ui/components";
 
 const createI18nContent = (t) => {
     const person = {
-        firstName: 'Selene',
-        lastName:  'Yu',
+        firstName: 'Calder Teo',
+        lastName:  'Teo',
         get name() {
             return `${this.firstName} ${this.lastName}`;
         },
         role:      t("person.role"),
         avatar:    '/images/avatar.jpg',
-        location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-        languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
+        location:  'Singapore',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+        languages: ['English']  // optional: Leave the array empty if you don't want to display languages
     }
 
     const newsletter = {
